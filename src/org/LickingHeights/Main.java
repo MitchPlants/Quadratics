45b565a31954a057837785057735d75b3419c588
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-26 09:57:19 -0400

    Declared Variables

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -5,13 +5,20 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //todo declare variables a, b, c, discriminant, axis of symmetry, parabla facing, x, y
+        double a,b,c,disciriminany,AxisOfSymmetry,x,y;
+        String ParabolaFacing;
         //todo initialize manually
+
         //todo calculations
+
         //todo print results
+
         //todo check accuracy
+
         //todo initialize for scanner and redo the initialized area
+
         //todo double check results
-        
+
 
 
 
