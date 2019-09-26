3632d4eb46e6e65aef1cd7deec1144bb65a4e5d2
Mitch Plants <mitche..plants.2023@student.lhschools.org>
2019-09-26 09:51:23 -0400

    Added todo steps

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -4,5 +4,45 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //todo declare variables a, b, c, discriminant, axis of symmetry, parabla facing, x, y
+        //todo initialize manually
+        //todo calculations
+        //todo print results
+        //todo check accuracy
+        //todo initialize for scanner and redo the initialized area
+        //todo double check results
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
